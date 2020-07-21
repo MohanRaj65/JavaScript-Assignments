0d554498b15ee913d3f0849c202dee2c4256ef41
MohanRaj65 <68269380+MohanRaj65@users.noreply.github.com>
2020-07-21 23:28:50 +0530

    Assignment Submitted for Day 5

--- JS Assignment Day-5/script/q2.js
+++ JS Assignment Day-5/script/q2.js
@@ -1,15 +1,54 @@
 console.log('q2');
 
-class use {
-    constructor(name,age,email){
-        this.name =name;
-        this.age =age;
-        this.email =email;
+class User {
+    constructor(name, age, email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.coins = 50;
+        this.course =[]
     }
-    login(){
+    login() {
         console.log(`${this.name} has logged in`);
     }
-    logout(){
+    logout() {
         console.log(`${this.name} has logged out`);
     }
 }
+
+class Moderator extends User {
+    addcoins(User) {
+        User.coins++;
+        console.log(`${User.name} have ${User.coins} coins`)
+    }
+    deletecoins(User) {
+        User.coins--;
+        console.log(`${User.name} have ${User.coins} coins`)
+    }
+}
+
+class Admin extends Moderator{
+    addcourse(User,course){
+        User.course.push(course);
+        console.log(User);
+
+    }
+    deletecourse(User,course){
+        User.course.pop(course);
+        console.log(User);
+    }
+}
+
+
+let user1 = new User('Mohan', 21, 'mohu@gmail.com')
+let user2 = new User('Raj', 22, 'raj@gmail.com')
+let mod = new Moderator('Rajini',25,'raji@gmail.com')
+let admin =new Admin("Kamal",30,'kamal@gmail.com'); 
+
+User=[user1,user2,mod,admin];
+
+mod.addcoins(user2);
+mod.deletecoins(user1);
+
+admin.addcourse(user1,'Javascript')
+admin.deletecourse(user1,'Javascript')
