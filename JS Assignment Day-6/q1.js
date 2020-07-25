54db571e597e1dac883db189f1307105586e10bd
MohanRaj65 <68269380+MohanRaj65@users.noreply.github.com>
2020-07-25 23:13:29 +0530

    Day6 Assignment submitted

---
+++ JS Assignment Day-6/q1.js
@@ -0,0 +1,16 @@
+console.log("Changing color of the document after 5 sec")
+
+let i =0;
+
+
+setInterval( function backgroundChange(){
+    colors = ["orange","red","blue","green"];
+    document.body.style.backgroundColor = `${colors[i]}`;
+    if ( i < colors.length){
+        i = i + 1;
+    }
+    if(i > colors.length -1)
+    {
+        i = 0;
+    }
+}, 5000);
---
+++ JS Assignment Day-6/q3.js
@@ -0,0 +1,26 @@
+console.log('q3');
+//Please comment the q1.js while running q3.js. 
+
+const name = (prompt('Enter your name', 'alpha'));
+rar.innerText += `Welcom to the Webpage ${name}`;
+
+function clock() {
+    let date = new Date();
+    let time = date.toLocaleTimeString();
+    clo.innerText = time;
+}
+
+setInterval(clock, 1000)
+
+const dar = document.getElementById('darkb');
+const night = document.getElementById('dark');
+dar.onclick = DarkMode = () => {
+    if (night.getAttribute('Dark') != "black") {
+        night.classList.toggle('nightmodeon');
+
+    }
+    else {
+        night.classList.toggle('nightmodeoff');
+    }
+
+}
